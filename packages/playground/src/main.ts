c98cf9e0d58b9e3a0f75981465b02e1e1529c091
ujiu <aisowe@sina.com>
2025-09-10 22:44:22 +0800

    publish the first version.

---
+++ packages/playground/src/main.ts
@@ -0,0 +1,4 @@
+import { add } from "@leishanglin/utils";
+
+// console.log(sayHello());
+console.log(12321, add(1, 3));
--- packages/utils/src/add.ts
+++ packages/utils/src/add.ts
@@ -1,3 +1,9 @@
+/**
+ * 求两个数字相加的和
+ * @param a 数字1
+ * @param b 数字2
+ * @returns a + b 的和
+ */
 export const add = (a: number, b: number) => {
   return a + b;
 };
